4ffbe152aa254f43bd0a8376707bb4133a9a5546
tenko <zhoumina@qq.com>
2017-11-14 17:20:40 +0800

    2017-11-14注释

--- Ch02/example1.py
+++ Ch02/example1.py
@@ -2,7 +2,8 @@
 
 import numpy
 import kNN
-datingDataMat,datingLabels = kNN.file2matrix('E:\VSCode\machinelearning\CH2\datingTestSet2.txt')
+datingDataMat,datingLabels = kNN.file2matrix('E:\VSCode\mymachinelearning\Ch02\datingTestSet2.txt')
+#file2matrix文件解析成matrix
 #写完整路径可以F5运行，不完整时只能用命令行python example1.py运行
 #datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
 import matplotlib
--- Ch02/example3.py
+++ Ch02/example3.py
@@ -2,12 +2,15 @@
 
 from numpy import array
 import kNN
-datingDataMat,datingLabels = kNN.file2matrix('E:\VSCode\machinelearning\CH2\datingTestSet2.txt') 
+datingDataMat,datingLabels = kNN.file2matrix('E:\VSCode\mymachinelearning\Ch02\datingTestSet2.txt') 
 #写完整路径可以F5运行，不完整时只能用命令行python example2.py运行
 import matplotlib
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(111)
+#add_subplot(111)又写作add_subplot(1,1,1),即add_subplot(i,j,n)表示把figure分为i行，j列的小方块，把图像画在第n块的地方。
 ax.axis([-5000,100000,-2,25])
+#axis调整坐标轴范围
 ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels))
+#scatter个性化设置 利用变量datingLabels存储的分类标签属性
 plt.show()
