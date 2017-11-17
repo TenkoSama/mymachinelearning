82158873a38c2be8ee3137caaa21bb1bb2cb0ccd
tenko <zhoumina@qq.com>
2017-11-17 17:19:57 +0800

    备注

--- Ch02/example3.py
+++ Ch02/example3.py
@@ -11,6 +11,8 @@ ax = fig.add_subplot(111)
 #add_subplot(111)又写作add_subplot(1,1,1),即add_subplot(i,j,n)表示把figure分为i行，j列的小方块，把图像画在第n块的地方。
 ax.axis([-5000,100000,-2,25])
 #axis调整坐标轴范围
-ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels))
+ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels),'o',alpha=0.5)
 #scatter个性化设置 利用变量datingLabels存储的分类标签属性
+#scatter(xData,yData,color,size,marker)
+#alpha透明度
 plt.show()
